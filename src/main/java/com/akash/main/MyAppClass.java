131d94f1152d364e5139d1b53925495bf608c00f
Harsh <sharsh1510@gmail.com>
2025-02-12 10:22:38 +0000

    pushing java code

---
+++ src/main/java/com/akash/main/MyAppClass.java
@@ -0,0 +1,17 @@
+package com.akash.main;
+
+public class MyAppClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		
+		
+		
+		for(int i = 1 ; i < 11 ; i++) {
+			System.out.println(i + " Hi this is my new feature...hahahahahahaha ..........");
+		}
+	}
+
+}
